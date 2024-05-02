a4adcd799c67b39e68ee1eb5ed85a01cf512745d
AZ <155894291+itsnotAZ@users.noreply.github.com>
2024-05-02 16:01:16 +0300

    Create installer.sh

---
+++ installer/installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# THE INSTALLER HAS CURL AS A DEPENDENCY!!
+
+echo "Downloading futurefetch..."
+curl https://raw.githubusercontent.com/itsnotAZ/futurefetch/main/futurefetch --output futurefetch
+sudo mv futurefetch /usr/bin
+sudo chmod +x futurefetch
+echo "Downloading config script"
+curl https://raw.githubusercontent.com/itsnotAZ/futurefetch/main/ffscriptbase/ffetchscript --output ffetchscript
+sudo mv ffetchscript /etc
+echo "Setup complete!"
+echo "To configure the ffetchscript located in /etc use a texteditor ran as super user."
+echo "Enjoy!"
